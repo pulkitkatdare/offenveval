cf6ef7ab28438cc21db266f83f7a2b0eb4989d0b
pulkitkatdare <yejimun2@illinois.edu>
2021-09-03 11:00:03 -0500

    Added the Cartpole Environment

---
+++ Cartpole/common/collect_data.py
@@ -0,0 +1,83 @@
+from Cartpole.common.replaybuffer import ReplayBuffer
+import numpy as np
+import _pickle as cPickle
+from pytorchrl.rl.models.cem.actor_cartpole import ActorNetwork
+import torch
+from torch.autograd import Variable
+
+
+def collect_data(simulation_env, deployment_env, episodes=1000, buffer_size=1000000):
+    D_P = ReplayBuffer(buffer_size=buffer_size)
+    steps = 0
+    actor = ActorNetwork(4, 2)
+    actor.load_state_dict(torch.load('pytorchrl/saver/cem_cartpole.ptr'))
+    for index in range(episodes):
+        print ("Episodes", index)
+        done = False
+        state = deployment_env.reset()
+        steps = 0
+        while (steps < 200):
+            if np.random.uniform() < 0.3:
+                action = np.random.choice([0, 1], p = [0.5, 0.5])
+            else:
+                input_state = np.reshape(state, (1, actor.state_dim))
+                input_state = torch.from_numpy(input_state)
+                dtype = torch.FloatTensor
+                input_state = Variable(input_state.type(dtype), requires_grad=False)
+                a = actor(input_state)
+                a = a.data.cpu().numpy()
+                action = a[0][0]
+            steps += 1
+            reward = 0
+            next_state, r, done, info = deployment_env.step(action)
+            if done:
+                break
+
+            reward += r
+            D_P.add(state, [action], next_state, reward, done)
+            state = next_state
+            
+    D_Q = ReplayBuffer(buffer_size=buffer_size)
+    steps = 0
+    for index in range(episodes):
+        print ("Episodes", index)
+        done = False
+        state = simulation_env.reset()
+        steps = 0
+        while (steps<200):
+            if np.random.uniform() < 0.3:
+                action = np.random.choice([0, 1], p=[0.5, 0.5])
+            else:
+                input_state = np.reshape(state, (1, actor.state_dim))
+                input_state = torch.from_numpy(input_state)
+                dtype = torch.FloatTensor
+                input_state = Variable(input_state.type(dtype), requires_grad=False)
+                a = actor(input_state)
+                a = a.data.cpu().numpy()
+                action = a[0][0]
+            steps += 1
+            reward = 0
+            next_state, r, done, info = simulation_env.step(action)
+            if done:
+                break
+            reward += r
+            D_Q.add(state, [action], next_state, reward, done)
+            state = next_state
+    return D_P, D_Q
+
+def save_model(D_P, D_Q, environment='GridWorld10x10'):
+    save_folder = 'Cartpole/environmentdata/' + environment + '/'
+    cPickle.dump(D_P, open(save_folder + 'DP.pkl', 'wb'))
+    cPickle.dump(D_Q, open(save_folder + 'DQ.pkl', 'wb'))
+
+def load_model(environment='GridWorld10x10'):
+    save_folder = 'Cartpole/environmentdata/' + environment + '/'
+    print (save_folder)
+    D_P = cPickle.load(open(save_folder + 'DP.pkl', 'rb'))
+    D_Q = cPickle.load(open(save_folder + 'DQ.pkl', 'rb'))
+    
+    return D_P, D_Q
+
+    
+    
+    
---
+++ Cartpole/model/BetaNet.py
@@ -0,0 +1,63 @@
+import math
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as weight_init
+from Cartpole.common.replaybuffer import ReplayBuffer
+
+
+
+class BetaNetwork(nn.Module):
+ 
+    def __init__(self, state_dim, action_bound, learning_rate, tau, seed, action_dim = 1):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_bound = action_bound
+        self.learning_rate = learning_rate
+        self.tau = tau
+        self.seed = seed
+
+        super(BetaNetwork, self).__init__()
+        torch.manual_seed(self.seed)
+        self.layer1 = nn.Linear(self.state_dim, 256)
+        torch.manual_seed(self.seed)
+        self.layer2 = nn.Linear(256, 512)
+        torch.manual_seed(self.seed)
+        self.layer3 = nn.Linear(512, 256)
+        torch.manual_seed(self.seed)
+        self.layer4 = nn.Linear(256, 1)
+        torch.manual_seed(self.seed)
+        
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr = self.learning_rate, weight_decay=0.00)
+
+    def forward(self, x):
+
+        y = F.relu(self.layer1(x))
+        y = F.relu(self.layer2(y))
+        y = F.relu(self.layer3(y))
+        y = torch.tanh(self.layer4(y))
+
+        scaled_y = torch.clamp(y*self.action_bound + self.action_bound, min=1e-5, max=10*self.action_bound)# + 1e-10#1e-5#torch.clamp(y, min=1e-5, max=self.action_bound) * 1.0#self.action_bound
+
+        return x, y, scaled_y
+
+    def predict(self, x):
+        x, y, scaled_y = self.forward(x)
+        return scaled_y
+
+    def train_step(self, states_p, states_q):
+        self.optimizer.zero_grad()
+        output1 = self.predict(states_q)
+        output1 = torch.mean((output1))
+        output2 = self.predict(states_p)
+        output2 = torch.mean(torch.log(output2))
+        output = output1 - output2
+        for param in self.parameters():
+            output += 0.5*self.tau * torch.norm(param) ** 2
+        output.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+  
+        return output
