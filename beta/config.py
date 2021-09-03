91863f47e61185ca9dd2b16b0b2b3f6388e62bb9
pulkitkatdare <yejimun2@illinois.edu>
2021-09-03 16:02:50 -0500

    Cleaned up code for beta with a config file

---
+++ beta/config.py
@@ -0,0 +1,29 @@
+import numpy as np 
+
+class Experiment(object):
+    def __init__(self, index = 0):
+        if index == 0:
+            self.mean_p = 4.0
+            self.mean_q = 4.0 
+            self.std_p = 2.0 
+            self.std_q = 1.0
+            self.action_bound = 40
+        elif index == 1:
+            self.mean_p = 3.0
+            self.mean_q = 4.0 
+            self.std_p = 2.0 
+            self.std_q = 1.0
+            self.action_bound = 20 
+        else:
+            self.mean_p = 3.0
+            self.mean_q = 4.0 
+            self.std_p = 2.0 
+            self.std_q = 1.0
+            self.action_bound = 20
+
+        self.sample_sizes = [500, 1000, 2000, 4000, 8000]
+        self.train = True
+        self.test = True
+        self.lr = 5e-6 
+        self.tau = 1e-4
+
--- experiments/train/beta.py
+++ experiments/train/beta.py
@@ -1,15 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Beta.BetaNet import BetaNetwork
+from beta.BetaNet import BetaNetwork
 import torch
 import tikzplotlib
+from beta.config import Experiment 
 
-BATCH_SIZE = 128
+colors = ['#000000', '#00441b', '#7f0000', '#084081', '#4d004b']
 
-def BetaEstimator(sample_size, data_p, data_q, train=True, key=''):
+
+def calculate_normalpdf(x, mean, variance):
+	first_term = 1/(np.sqrt(2*np.pi)*np.sqrt(variance))
+	second_term = np.exp(-0.5*(((x-mean)**2)/variance))
+	return first_term*second_term
+
+
+def BetaEstimator(sample_size, data_p, data_q, config, train, key):
 
     batch_size, dimension = np.shape(data_p)
-    Beta = BetaNetwork(state_dim=dimension, action_bound=40.0, learning_rate=5e-6, tau=1e-4, seed=np.random.randint(0, 1000), action_dim=1) #10.0 # BetaNet(n_gaussians = batch_size)
+    Beta = BetaNetwork(state_dim=dimension, action_bound=config.action_bound, learning_rate=config.lr, tau=config.tau, seed=np.random.randint(0, 1000), action_dim=1) 
 
     if train:
         for i in range(int(sample_size)):
@@ -29,45 +37,43 @@ def BetaEstimator(sample_size, data_p, data_q, train=True, key=''):
             if i % 100 == 0:
                 print("Training Iterations", i, output_beta.data.numpy())
 
-        with open('./Beta/data/Baselines/beta_' + str(sample_size) + key + '.ptr', 'wb') as output:
+        with open('./beta/data/beta_' + str(sample_size) + key + '.ptr', 'wb') as output:
                     torch.save(Beta.state_dict(), output)
-
-
+                    
     else:
-        Beta.load_state_dict(torch.load('./Beta/data/Baselines/beta_' + str(sample_size) + key + '.ptr'))
+        Beta.load_state_dict(torch.load('./beta/data/beta_' + str(sample_size) + key + '.ptr'))
+        
     return Beta
 
-def calculate_normalpdf(x, mean, variance):
-	first_term = 1/(np.sqrt(2*np.pi)*np.sqrt(variance))
-	second_term = np.exp(-0.5*(((x-mean)**2)/variance))
-	return first_term*second_term
+
 
 if __name__ == '__main__':
 
-    train = False
-    test = True
-    mean_p = 2.0
-    variance_p = 1.0
-    mean_q = 4.0
-    variance_q = 2.0
-
-    colors = ['#000000', '#00441b', '#7f0000', '#084081', '#4d004b']#['#9e9ac8', '#807dba',
+    config = Experiment(index=0)
+    mean_p = config.mean_p
+    variance_p = config.std_p
+    mean_q = config.mean_q
+    variance_q = config.std_q
 
     X = np.linspace(-4, 8, 100)
-    Sample_Sizes = [2000, 4000, 8000, 'Baselines']#[2000, 4000, 8000, 10000, 15000, 'Baselines']#, 1000, 20000]
+    sample_size = config.sample_sizes
+
+    train = config.train 
+    test = config.test
 
     if train:
-        for sample in Sample_Sizes:
+        for sample in sample_size:
             data_p = np.random.normal(loc=mean_p, scale=np.sqrt(variance_p), size=(sample, 1))
             data_q = np.random.normal(loc=mean_q, scale=np.sqrt(variance_q), size=(sample, 1))
             for i in range(10):
-                key = '_' + str(i) + '_2142'
-                Beta = BetaEstimator(sample_size=sample, data_p=data_p, data_q=data_q, train=True, key=key)
+                key = '_' + str(i) + '_' + str(int(mean_p)) + str(int(variance_p)) + str(int(mean_q)) + str(int(variance_q))
+                Beta = BetaEstimator(sample_size=sample, data_p=data_p, data_q=data_q, train=True, key=key, config=config)
     if test:
         data = np.zeros((10, 100))
         baseline = []
+        sample_size.append('Baselines')
         index = 0
-        for sample in Sample_Sizes:
+        for sample in sample_size:
             print (sample)
 
             j = -1
@@ -83,7 +89,7 @@ if __name__ == '__main__':
                     data_p = np.random.normal(loc=mean_p, scale=np.sqrt(variance_p), size=(sample, 1))
                     data_q = np.random.normal(loc=mean_q, scale=np.sqrt(variance_q), size=(sample, 1))
                     for i in range(10):
-                        key = '_' + str(i) + '_2142'
+                        key = '_' + str(i) + '_' + str(int(mean_p)) + str(int(variance_p)) + str(int(mean_q)) + str(int(variance_q))
                         Beta = BetaEstimator(sample_size=sample, data_p=data_p, data_q=data_q, train=False, key=key)
                         input = np.expand_dims([point], axis=1)
                         input = torch.from_numpy(input)
@@ -91,44 +97,23 @@ if __name__ == '__main__':
                         output = Beta.predict(input)
                         output = output.data.numpy()
                         data[i, int(j)] = output[0, 0]
-                    mean.append(np.mean(data[:, j]));
-                    var.append(np.var(data[:, j]));
+                    mean.append(np.mean(data[:, j]))
+                    var.append(np.var(data[:, j]))
             if sample != 'Baselines':
-                mean = np.array(mean);
-                var = np.array(var);
-                print (var)
-                plt.plot(X, mean, linewidth=2, color=colors[index]);
+                mean = np.array(mean)
+                var = np.array(var)
+                plt.plot(X, mean, linewidth=2, color=colors[index])
                 plt.fill_between(X, mean-1*var, mean+1*var, color=colors[index], alpha=0.2)
             index += 1
         plt.plot(X, baseline, '--', linewidth=2, color='#4a1486')
-        #plt.plot(data_p, np.zeros(8000), 'x')
-        #plt.plot(data_q, np.zeros(8000) + 130, 'o')
         legends = []
-        Sample_Sizes.remove('Baselines')
+        sample_sizes.remove('Baselines')
         for i in range(len(Sample_Sizes)):
-                legends.append(str(Sample_Sizes[i]))
+                legends.append(str(sample_sizes[i]))
         legends.append('Oracle')
-        #legends.append('P-samples')
-        #legends.append('Q-samples')
         plt.legend(legends)
         plt.grid(True)
-        tikzplotlib.save("beta.tex")
-        plt.savefig('beta.png', dpi=300)
-#plt.show()
-#
-#
-#	for index, sample_size in enumerate(Sample_Sizes):
-#
-#		data_p = np.random.normal(loc=mean_p, scale=np.sqrt(variance_p), size=(sample_size, 1))
-#		data_q = np.random.normal(loc=mean_q, scale=np.sqrt(variance_q), size=(sample_size, 1))
-#		for i in range(10):
-#			key = '_' + str(i)
-#			Beta = BetaEstimator(sample_size=sample_size, data_p=data_p, data_q=data_q, key=key)
-#	'''
-
-
-
-
-
-
+        key = str(int(mean_p)) + str(int(variance_p)) + str(int(mean_q)) + str(int(variance_q))
+        tikzplotlib.save('./beta/assets/beta_' + key +  '.tex')
+        plt.savefig('./beta/assets/beta_' + key + '.png', dpi=300)
 
